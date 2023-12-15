af0f3a4f7b690b54a94b3f15d0b768e155315f24
declan.chen <declan.chen@funplus.com>
2023-12-15 15:42:40 +0800

    update verify basic erc721c for mainnet

--- scripts/verify/verify-basic-erc721c.ts
+++ scripts/verify/verify-basic-erc721c.ts
@@ -2,13 +2,13 @@ import hre from "hardhat";
 
 const main = async () => {
   await hre.run("verify:verify", {
-    address: "0xc42F1C6814566Cf5Ae10f268E9339E4f97C622ee",
+    address: "0xFdF5aCd92840E796955736B1BB9CC832740744Ba",
     constructorArguments: [
-      "MechPal NFT",
-      "MECHPAL",
-      "https://api.xterio.net/asset/nft/meta/opbnb_testnet",
-      "0x5d3757bC0f724aA4332DCa2184edA1b8a94eA0b6", // "0xC8d6b1D3Cca37952465086D9D96DB0E1C96f4E1e",
-      "0x3994FfB545A10F189F9C0Ed1a58f0e1D23f22364", // "0x62d49Da6233962e7BEd1C3E9dA3e56739b014c42",
+      "OVERWORLD INCARNA",
+      "INCARNA",
+      "https://api.xter.io/asset/nft/meta/ethereum",
+      "0x7127f0FEaEF8143241A5FaC62aC5b7be02Ef26A9",
+      "0xeCF63dFBa014dc9CEa2715d10082Bbbc892D2188",
     ],
     contract: "contracts/basic-tokens/BasicERC721C.sol:BasicERC721C",
   });
