6b764ef6927d035c554fc1643908be95a5cea99b
declan.chen <declan.chen@funplus.com>
2024-03-28 17:47:12 +0800

    verify-basic-erc1155c

--- scripts/verify/verify-basic-erc1155c.ts
+++ scripts/verify/verify-basic-erc1155c.ts
@@ -2,11 +2,11 @@ import hre from "hardhat";
 
 const main = async () => {
   await hre.run("verify:verify", {
-    address: "0x59c5fb12E3b822cC68873805720B8874Af603DC2",
+    address: "0xa90319054b3210e9a81740D8e8520c4D52B428d4",
     constructorArguments: [
-      "https://api.xter.io/asset/nft/meta/xterio_testnet",
-      "0xB6Fe7Bc1c8836983C0643D5869c42bD27aCAAedD",
-      "0xB4c7E393619E0924e6B3dbc718B7e2a29A123529"
+      "https://api.xter.io/asset/nft/meta/xterio",
+      "0x7127f0FEaEF8143241A5FaC62aC5b7be02Ef26A9",
+      "0xeCF63dFBa014dc9CEa2715d10082Bbbc892D2188"
     ],
     contract: "contracts/basic-tokens/BasicERC1155C.sol:BasicERC1155C",
   });
