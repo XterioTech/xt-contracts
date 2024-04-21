7d4a6bab3ed9aaae81f8f623047f530180261bd1
declan.chen <declan.chen@funplus.com>
2024-04-21 11:35:53 +0800

    verify deposit on mainnet

--- scripts/verify/verify-deposit-raffle-minter.ts
+++ scripts/verify/verify-deposit-raffle-minter.ts
@@ -2,18 +2,18 @@ import hre from "hardhat";
 
 const main = async () => {
   await hre.run("verify:verify", {
-    address: "0x32cF51D61247E787E61cA956bec3cA4e5c345B6a",
+    address: "0x9131e8F8ee1Fef30B00B4146017d56B24A30d5e0",
     constructorArguments: [
       "0x6F272C3b23Fc0525b6696aF4405434c3c10C7c26", // address _admin,
       "0xB6Fe7Bc1c8836983C0643D5869c42bD27aCAAedD", // address _gateway,
-      "0x015C1E162b632EeF0845431a8B21061CAEbC0EEe", // address _nftAddress,
-      "0x1371485360c7F5B8336db38A446C5fD8cf084243", // address _paymentRecipient,
-      1713427200, // uint256 _auctionStartTime,
-      1713499200, // uint256 _auctionEndTime,
-      "10000000000000000", // uint256 _unitPrice,
+      "0x41009de01cDD0E3BD907f6778Ad7EF6e062ef576", // address _nftAddress,
+      "0xcB907bE1d76b3E7672ea6742ccC5fcDbE9bb3530", // address _paymentRecipient,
+      1713758400, // uint256 _auctionStartTime,
+      1714363200, // uint256 _auctionEndTime,
+      "300000000000000000", // uint256 _unitPrice,
       5, // uint256 _maxShare,
       0, // uint256 _nftPrice,
-      10, // uint256 _nftAmount
+      945, // uint256 _nftAmount
     ],
     contract: "contracts/launchpad/DepositRaffleMinter.sol:DepositRaffleMinter",
   });
