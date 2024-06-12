15f0c52cc9490b9a6cd61af2be1af22f6081743e
Francis <710892027dongyi@gmail.com>
2024-06-12 18:07:30 +0800

    vault

--- contracts/airdrop/WhitelistClaimERC20.sol
+++ contracts/airdrop/WhitelistClaimERC20.sol
@@ -8,17 +8,22 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract WhitelistClaimERC20 is WhitelistClaim {
     using SafeERC20 for IERC20;
     IERC20 public token;
+    address public vault = address(this);
 
     constructor(
         bytes32 _merkleRoot,
         uint256 _deadline,
-        address _token
+        address _token,
+        address _vault
     ) WhitelistClaim(_merkleRoot, _deadline) {
         token = IERC20(_token);
+        if (_vault != address(0)) {
+            vault = _vault;
+        }
     }
 
     function _payOut(uint256 amount, address to) internal override {
-        token.safeTransfer(to, amount);
+        token.safeTransferFrom(vault, to, amount);
     }
 
     function _withdraw(address to) internal override {
