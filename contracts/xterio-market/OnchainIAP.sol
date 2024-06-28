cea50ebdebf3989d664177807b6852801df0ecdf
Francis <710892027dongyi@gmail.com>
2024-06-28 23:35:08 +0800

    rename OnchainIAP

--- contracts/xterio-market/OnchainIAP.sol
+++ contracts/xterio-market/OnchainIAP.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-contract XterioMarket is AccessControl {
+contract OnchainIAP is AccessControl {
     using SafeERC20 for IERC20;
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
