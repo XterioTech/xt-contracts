ae77461a299a728212de1f24920bcd6a4b7aefef
Francis <710892027dongyi@gmail.com>
2024-07-19 21:44:45 +0800

    sol 0.8.20

--- contracts/fans-create-upgradeable/FansCreateERC20Upgradeable.sol
+++ contracts/fans-create-upgradeable/FansCreateERC20Upgradeable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import "./FansCreateCoreUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
