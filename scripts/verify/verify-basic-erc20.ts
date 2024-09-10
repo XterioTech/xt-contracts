a1e60ed612b6ecdaf02f291513c298f85f9864a7
declan <declan@libecc.io>
2024-09-10 17:24:49 +0800

    configure new gateway addr for xterioTestnet

--- scripts/verify/verify-basic-erc20.ts
+++ scripts/verify/verify-basic-erc20.ts
@@ -2,12 +2,12 @@ import hre from "hardhat";
 
 const main = async () => {
   await hre.run("verify:verify", {
-    address: "0xF4ECC1C74D120649f6598C7A217AbaFfdf76Cd4F",
+    address: "0x94F8D460A89949044acaa0a3dc6991AAbEBDC688",
     constructorArguments: [
-      "Dino Amber",
-      "DAM",
+      "Dino Bucks",
+      "DB",
       8,
-      "0x7127f0FEaEF8143241A5FaC62aC5b7be02Ef26A9",
+      "0x7127f0feaef8143241a5fac62ac5b7be02ef26a9",
       "0xeCF63dFBa014dc9CEa2715d10082Bbbc892D2188",
     ],
     contract: "contracts/basic-tokens/BasicERC20.sol:BasicERC20",
