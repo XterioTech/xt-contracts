8b6630dfae692dabde0e3213fc576464335138e0
zhichunqi <zhichunqi@zhichunqideMacBook-Pro.local>
2024-09-12 16:50:29 +0800

    Add querys funcs

--- contracts/checkIn/CheckIn.sol
+++ contracts/checkIn/CheckIn.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.20;
 contract CheckInContract {
     uint256 public constant SECONDS_IN_DAY = 86400;
     uint256 public immutable startTime;
-    mapping(address who => mapping(uint256 channel => mapping(uint256 dayIndex => bool checked))) records;
+    mapping(address user => mapping(uint256 channel => mapping(uint256 dayIndex => bool checked))) records;
 
     event CheckIn(
-        address indexed who,
+        address indexed user,
         uint256 indexed channel,
         uint256 dayIndex
     );
@@ -23,7 +23,7 @@ contract CheckInContract {
     }
 
     function query(
-        address _who,
+        address _user,
         uint256 _channel,
         uint256 _timestamp
     ) external view returns (bool) {
@@ -31,6 +31,36 @@ contract CheckInContract {
             return false;
         }
         uint256 dayIndex = (_timestamp - startTime) / SECONDS_IN_DAY + 1;
-        return records[_who][_channel][dayIndex];
+        return records[_user][_channel][dayIndex];
+    }
+
+    function querysForUser(
+        address _user,
+        uint256 _channel,
+        uint256[] calldata _timestamps
+    ) external view returns (bool[] memory) {
+        bool[] memory results = new bool[](_timestamps.length);
+        for (uint256 i; i < _timestamps.length; ) {
+            results[i] = this.query(_user, _channel, _timestamps[i]);
+            unchecked {
+                ++i;
+            }
+        }
+        return results;
+    }
+
+    function querysForUsers(
+        address[] calldata _users,
+        uint256 _channel,
+        uint256 _timestamp
+    ) external view returns (bool[] memory) {
+        bool[] memory results = new bool[](_users.length);
+        for (uint256 i; i < _users.length; ) {
+            results[i] = this.query(_users[i], _channel, _timestamp);
+            unchecked {
+                ++i;
+            }
+        }
+        return results;
     }
 }
--- test/checkIn/CheckIn.ts
+++ test/checkIn/CheckIn.ts
@@ -7,8 +7,8 @@ describe("CheckIn", function () {
     const gameChannel = 1;
     const tgChannel = 2;
     async function basicFixture() {
-        const [deployer, user1, user2] = await hre.ethers.getSigners();
-        const startTime = Number("1726099200"); // 这里应该使用具体某一天的 utc 零点的时间作为基准
+        const [user1, user2] = await hre.ethers.getSigners();
+        const startTime = Math.floor(Date.now() / 1000 - 10); // 这里应该使用具体某一天的 utc 零点的时间作为基准
         const checkIn = await deployCheckIn(startTime);
 
         return { user1, user2, checkIn, startTime };
@@ -29,8 +29,10 @@ describe("CheckIn", function () {
 
             const currentBlock = await hre.ethers.provider.getBlock("latest");
             const currentTime = currentBlock?.timestamp || 0;
-            console.log("currnet block timestamp: ", currentTime);
+            console.log("startTime: ", startTime);
+            console.log("first day block timestamp: ", currentTime);
 
+            // user1 第一天签到
             await checkIn.connect(user1).checkIn(gameChannel);
 
             expect(await checkIn.query(await user1.getAddress(), gameChannel, currentTime)).to.equal(true);
@@ -51,6 +53,7 @@ describe("CheckIn", function () {
             expect(await checkIn.query(await user1.getAddress(), gameChannel, timestamp1)).to.equal(false);
             //
 
+            // user2 第一天签到
             await checkIn.connect(user2).checkIn(tgChannel);
 
             expect(await checkIn.query(await user2.getAddress(), tgChannel, currentTime)).to.equal(true);
@@ -72,16 +75,16 @@ describe("CheckIn", function () {
 
             //
             // 手动调节区块到第二天的时间
-            let timestamp2 = startTime + 60 * 60 * 24
+            let timestamp2 = startTime + 60 * 60 * 24 + 10
             await hre.ethers.provider.send("evm_setNextBlockTimestamp", [timestamp2])
             await hre.ethers.provider.send("evm_mine", []);
             const currentBlock2 = await hre.ethers.provider.getBlock("latest");
             const currentTime2 = currentBlock2?.timestamp || 0;
-            console.log("second day block timestamp: ", timestamp2, currentTime2);
+            console.log("second day block timestamp: ", currentTime2);
 
-            // 第二天签到
+            // user1 第二天签到
             await checkIn.connect(user1).checkIn(gameChannel);
-            // 第一天的检查
+            // user1 第一天的检查
             expect(await checkIn.query(await user1.getAddress(), gameChannel, currentTime)).to.equal(true);
             timestamp1 = startTime + 60 * 60 * 5; // 使用过了 5 小时的时间戳查询
             expect(await checkIn.query(await user1.getAddress(), gameChannel, timestamp1)).to.equal(true);
@@ -96,8 +99,8 @@ describe("CheckIn", function () {
             timestamp1 = startTime - 60 * 60 * 1; // 使用过去 1 小时的时间戳查询，即前一天时间
             expect(await checkIn.query(await user1.getAddress(), gameChannel, timestamp1)).to.equal(false);
 
-            // 第二天的检查
-            expect(await checkIn.query(await user1.getAddress(), gameChannel, currentTime)).to.equal(true);
+            // user1 第二天的检查
+            expect(await checkIn.query(await user1.getAddress(), gameChannel, currentTime2)).to.equal(true);
             timestamp1 = timestamp2 + 60 * 60 * 5; // 使用过了 5 小时的时间戳查询
             expect(await checkIn.query(await user1.getAddress(), gameChannel, timestamp1)).to.equal(true);
             timestamp1 = timestamp2 + 60 * 60 * 10; // 使用过了 10 小时的时间戳查询
@@ -110,6 +113,31 @@ describe("CheckIn", function () {
 
             timestamp1 = timestamp2 + 60 * 60 * 24; // 使用过了 24 小时的时间戳查询
             expect(await checkIn.query(await user1.getAddress(), gameChannel, timestamp1)).to.equal(false);
+
+
+            // 查询 user1 第一和第二天的签到情况
+            let firstDay = currentTime + 60 * 60 * 1;
+            let secondDay = currentTime2 + 60 * 60 * 1;
+            const results = await checkIn.querysForUser(await user1.getAddress(), gameChannel, [firstDay, secondDay]);
+            for (let result of results) {
+                expect(result).to.equal(true);
+            }
+
+            //  查询 user1, user2 第一天
+            const results2 = await checkIn.querysForUsers([await user1.getAddress(), await user2.getAddress()], gameChannel, firstDay)
+            // user2 是 tg 签到，所以 game 这里的 false
+            expect(results2[0]).to.equal(true);
+            expect(results2[1]).to.equal(false);
+
+            const results3 = await checkIn.querysForUsers([await user1.getAddress(), await user2.getAddress()], tgChannel, firstDay)
+            expect(results3[0]).to.equal(false);
+            expect(results3[1]).to.equal(true);
+
+            //  查询 user1, user2 第二天
+            const results4 = await checkIn.querysForUsers([await user1.getAddress(), await user2.getAddress()], gameChannel, secondDay)
+            // user2 第二天没有签到
+            expect(results4[0]).to.equal(true);
+            expect(results4[1]).to.equal(false);
         });
     });
 });
