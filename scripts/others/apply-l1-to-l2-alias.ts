77629831a4dbb41f8383e935925f91adca52f95a
yycceth <zhichunqi93@gmail.com>
2025-01-03 11:17:25 +0800

    Modify scripts/others/apply-l1-to-l2-alias.ts

--- scripts/others/apply-l1-to-l2-alias.ts
+++ scripts/others/apply-l1-to-l2-alias.ts
@@ -7,7 +7,7 @@ const main = async () => {
     const l1AddressNumber = BigInt("0x" + l1Address.slice(2));
     const result = l1AddressNumber + offsetAddressNumber;
     const l2Address = "0x" + result.toString(16).slice(-40);
-    console.log("l1Address =", l1Address);
+    console.log("l1Address =", hre.ethers.getAddress(l1Address));
     console.log("applyL1ToL2Alias");
     console.log("l2Address =", hre.ethers.getAddress(l2Address));
 
